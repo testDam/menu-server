bdae233424ae8ed6e374e9cf13ffcace5ade2f0c
PUPIE <dpeka8520@gmail.com>
2020-06-22 20:46:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const DB = require('./db');
 
 app.listen(process.env.PORT || 8080)
 
-    app.get('/test', (req, res) => {        
+    app.get('/', (req, res) => {        
 
         res.send({
-        	"text": "Did you call me?",
-	"responseType": "inChannel",
-            "attachments": [
+        	text: 'Did you call me?',
+	responseType: 'inChannel',
+            attachments: [
               {
-                "title": "Silicon Park Weekly Menu Table",
-                "imageUrl": "https://img9.yna.co.kr/etc/inner/KR/2019/04/08/AKR20190408066300073_01_i_P2.jpg"
+                title: 'Silicon Park Weekly Menu Table',
+                imageUrl: 'https://img9.yna.co.kr/etc/inner/KR/2019/04/08/AKR20190408066300073_01_i_P2.jpg'
               }
             ]
         })
