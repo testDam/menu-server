6e9114e1769e48d2f822eedae1701b388d820ed8
PUPIE <dpeka8520@gmail.com>
2020-06-22 21:21:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.listen(process.env.PORT || 8080)
     app.get('/', (req, res) => {        
 
         res.send({
-        	text: 'Did you call me?',
+        	text: 'Weekly Menu Table',
 	responseType: 'inChannel',
             attachments: [
               {
                 title: 'Silicon Park Weekly Menu Table',
-                imageUrl: 'https://img9.yna.co.kr/etc/inner/KR/2019/04/08/AKR20190408066300073_01_i_P2.jpg'
+                imageUrl: 'https://ifh.cc/g/6tZJBk.jpg'
               }
             ]
         })
