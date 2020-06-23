f910c800d1c8ce9a3aefa62ab64a1452f0298900
PUPIE <dpeka8520@gmail.com>
2020-06-23 13:55:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var app = express()
-const DB = require('./db');
+var request = require('request');
+
+var headers = {
+    'Content-Type': 'application/json'
+};
+
+var dataString = '{"botName": "주간메뉴를 알리는 Bot", "botIconImage": "https://thumbs.gfycat.com/QuarrelsomeDismalBobolink-size_restricted.gif", "attachments" : [{"title" : "실리콘파크 식당 주간메뉴 (06.22~06.29)","titleLink" : "http://whatsup.nhnent.com/shortCut/boardArticle/299928","text" : "이번주 메뉴입니다.","imageUrl": "https://i.ibb.co/c8sk091/aadf8cc0-854d-4a14-a1c2-63c4762f17e3.png"}]}';
+
+var options = {
+    url: '$hook_url',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
 
 app.listen(process.env.PORT || 8080)
 
-    app.get('/', (req, res) => {        
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
 
-        res.send({hook_url=https://hook.dooray.com/services/2141235803690805491/2773112445718757478/N8oGtQBiRBGCvxokp7G6Tw
-curl -H "Content-Type: application/json" -X POST -d '{"botName": "주간메뉴를 알리는 Bot", "botIconImage": "https://thumbs.gfycat.com/QuarrelsomeDismalBobolink-size_restricted.gif", "attachments" : [{"title" : "실리콘파크 식당 주간메뉴 (06.22~06.29)","titleLink" : "http://whatsup.nhnent.com/shortCut/boardArticle/299928","text" : "이번주 메뉴입니다.","imageUrl": "https://i.ibb.co/c8sk091/aadf8cc0-854d-4a14-a1c2-63c4762f17e3.png"}]}' $hook_url})
-    });
+request(options, callback);
