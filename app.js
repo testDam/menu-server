eac44d9ebd48daa9e4a52e9f99b302ceaa96dd1a
PUPIE <dpeka8520@gmail.com>
2020-06-23 13:35:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,5 @@ app.listen(process.env.PORT || 8080)
 
     app.get('/', (req, res) => {        
 
-        res.send({
-        	text: 'Weekly Menu Table',
-	responseType: 'inChannel',
-            attachments: [
-              {
-                title: 'Silicon Park Weekly Menu Table',
-                imageUrl: 'https://ifh.cc/g/6tZJBk.jpg'
-              }
-            ]
-        })
+        res.send({"text": "Weekly Menu Table","responseType": "inChannel","attachments" : [{"title" : "실리콘파크 식당 주간메뉴 (06.22~06.29)","titleLink" : "http://whatsup.nhnent.com/shortCut/boardArticle/299928","text" : "이번주 메뉴입니다.","imageUrl": "https://i.ibb.co/c8sk091/aadf8cc0-854d-4a14-a1c2-63c4762f17e3.png"}]})
     });
